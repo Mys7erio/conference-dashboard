cd57a70bc93dd05f54111fa282c166ebe0e03a13
MOJO <abhisatish75@gmail.com>
2023-07-06 13:40:00 +0530

    changes in SuperAdminSidebar

--- src/Components/SuperAdminSidebar.jsx
+++ src/Components/SuperAdminSidebar.jsx
@@ -4,8 +4,6 @@ import { Sidebar, Menu, MenuItem, SubMenu} from 'react-pro-sidebar';
 import { FaBuilding, FaBars, FaUserPlus, FaUserTag, FaChartLine,FaUserEdit, FaWalking, FaBell, FaWrench} from 'react-icons/fa';
 import Logo from '../assets/KJCLogo.png';
 import userimage from '../assets/userimage.png'
-import { FaHome, FaBars} from 'react-icons/fa';
-import Logo from '../assets/KJCLogo.png';
 import '../Components/SuperAdminSidebar.css' 
 const SuperAdminSidebar = () => {
   const [collapsed, setCollapsed] = React.useState(false);
