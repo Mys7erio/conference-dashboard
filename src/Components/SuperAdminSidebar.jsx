f4d87d9acbf4fdfa7f70732cb76977a531300564
shambhavi <shambhavivijay9@gmail.com>
2023-07-06 22:07:21 +0530

    create admin form created

--- src/Components/SuperAdminSidebar.jsx
+++ src/Components/SuperAdminSidebar.jsx
@@ -4,8 +4,7 @@ import { Sidebar, Menu, MenuItem, SubMenu} from 'react-pro-sidebar';
 import { FaBuilding, FaBars, FaUserPlus, FaUserTag, FaChartLine,FaUserEdit, FaWalking} from 'react-icons/fa';
 import Logo from '../assets/KJCLogo.png';
 import userimage from '../assets/userimage.png'
-import { FaHome, FaBars} from 'react-icons/fa';
-import Logo from '../assets/KJCLogo.png';
+import { FaHome} from 'react-icons/fa';
 import '../Components/SuperAdminSidebar.css' 
 const SuperAdminSidebar = () => {
   const [collapsed, setCollapsed] = React.useState(false);
@@ -32,7 +31,9 @@ const SuperAdminSidebar = () => {
           </div>
           <hr />
         <MenuItem icon={<FaBuilding />} component={<Link to="/AddInstitution"/>}>Create Institutions</MenuItem>
-        <MenuItem icon={<FaUserPlus />} component={<Link to="/CreateAdmin"/>}>Create Admin</MenuItem>
+        <MenuItem icon={<FaUserPlus />}>
+            <Link to="/CreateAdmin">Create Admin</Link>
+          </MenuItem>
         <MenuItem icon={<FaUserTag />} component={<Link to="/AssignAdmin"/>}>Assign Admin</MenuItem>
         <MenuItem icon={<FaChartLine />} component={<Link to="/SuperAdminDashboard"/>}>Dashboard</MenuItem>
         <MenuItem icon={<FaUserEdit />} component={<Link to="/UserManagement"/>}>User Management</MenuItem>
