4b385d5003d270e5fb9cead6631f42793bafee49
MOJO <abhisatish75@gmail.com>
2023-07-06 11:05:25 +0530

    made changes in the SuperAdminSidebar Design

--- src/Components/SuperAdminSidebar.jsx
+++ src/Components/SuperAdminSidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import { Sidebar, Menu, MenuItem, SubMenu} from 'react-pro-sidebar';
-import { FaBuilding, FaBars, FaUserPlus, FaUserTag, FaChartLine,FaUserEdit, FaWalking} from 'react-icons/fa';
+import { FaBuilding, FaBars, FaUserPlus, FaUserTag, FaChartLine,FaUserEdit, FaWalking, FaBell, FaWrench} from 'react-icons/fa';
 import Logo from '../assets/KJCLogo.png';
 import userimage from '../assets/userimage.png'
 import '../Components/SuperAdminSidebar.css' 
@@ -9,7 +9,7 @@ const SuperAdminSidebar = () => {
   const [collapsed, setCollapsed] = React.useState(false);
   return (  
     <div style={{ display: 'flex', height: '100%', minHeight: '400px'}}>
-    <Sidebar collapsed={collapsed} className='custom' backgroundColor="#66615B" collapsedWidth="50px">
+    <Sidebar collapsed={collapsed} className='custom' backgroundColor="#66615B" collapsedWidth="50px" transitionDuration={1000}>
       <Menu>
           <div className="topLogo">
             <div className="logo">
@@ -34,9 +34,18 @@ const SuperAdminSidebar = () => {
         <MenuItem icon={<FaUserTag />} component={<Link to="/AssignAdmin"/>}>Assign Admin</MenuItem>
         <MenuItem icon={<FaChartLine />} component={<Link to="/SuperAdminDashboard"/>}>Dashboard</MenuItem>
         <MenuItem icon={<FaUserEdit />} component={<Link to="/UserManagement"/>}>User Management</MenuItem>
-        <button className='logout'>
-          <FaWalking /> Logout
+        <div className="SuperAdminSidebarEnd">
+        <button className='NotificationButton SidebarButtons'>
+          <FaBell /> Notifications
         </button>
+        <button className='SettingsButton SidebarButtons'>
+          <FaWrench /> Settings
+        </button>
+        <button className='LogoutButton SidebarButtons'>
+          <FaWalking /> Log out
+        </button>
+        </div>
+        
       </Menu>
     </Sidebar>
     <main style={{ padding: 10 }}>  
